50e566a67c1a89df4151224c36f8e0b0f7391883
Vaibhav Ganju <vaibhav34@gmail.com>
2019-09-04 23:14:32 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let Blockchain = require('./blockchain');
 let Transaction = require('./transaction');
 let transactions = [];
 
+let genesisBlock = new Block();
+let blockchain = new Blockchain(genesisBlock);
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -14,25 +17,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/blockchain', (req, res) => {
-    let genesisBlock = new Block();
-    let blockchain = new Blockchain(genesisBlock);
-    let transaction = new Transaction('Mary', 'John', 100);
-    let block1 = blockchain.getNextBlock([transaction]);
-    blockchain.addBlock(block1);
+    // let genesisBlock = new Block();
+    // let blockchain = new Blockchain(genesisBlock);
+    // let transaction = new Transaction('Mary', 'John', 100);
+    // let block1 = blockchain.getNextBlock([transaction]);
+    // blockchain.addBlock(block1);
 
     res.json(blockchain)
+})  
+
+app.get('/mine', (req, res) => {
+    const block = blockchain.getNextBlock(transactions);
+    blockchain.addBlock(block);
+    res.json(block);
 })
 
 app.post('/transactions', (req, res) => {
     const { to, from, amount } = req.body;
     let transaction = new Transaction(from, to, amount);
-
     transactions.push(transaction);
-
-    console.log(req.body.to)
-    console.log(req.body.from)
-    console.log(req.body.amount)
-
     res.json(transactions)
 })
 
